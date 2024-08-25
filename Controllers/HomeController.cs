8bc97f05f2111a1cae564b7131f53a03e4e4f6df
Enrique Paco <enriquemiguel4242@gmail.com>
2024-08-25 12:24:52 -0500

    estructura

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,27 +5,19 @@ using PC1.Models;
 namespace PC1.Controllers;
 
 public class HomeController : Controller
-{
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
     {
-        _logger = logger;
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public IActionResult Operacion()
+        {
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
+        public IActionResult Acerca()
+        {
+            return View();
+        }
     }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-}
